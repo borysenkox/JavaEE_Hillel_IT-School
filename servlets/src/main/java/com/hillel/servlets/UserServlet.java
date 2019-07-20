671fcb4b904341d14dadfc36ac47070756b232e1
Sergey <borisenkosergei@gmail.com>
2019-07-20 14:00:50 +0300

    TASK-3: Added log4j and fixed storage mapping in UserServlet

--- servlets/src/main/java/com/hillel/servlets/UserServlet.java
+++ servlets/src/main/java/com/hillel/servlets/UserServlet.java
@@ -15,10 +15,12 @@ import com.hillel.service.UserService;
 import com.hillel.util.JsonUtil;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
 @WebServlet("/user")
 @Setter
 @Getter
+@Log4j
 public class UserServlet extends HttpServlet {
     private UserDao userDao;
     private UserService userService;
@@ -26,13 +28,14 @@ public class UserServlet extends HttpServlet {
 
     @Override
     public void init() {
-        userDao = (UserDao) getServletContext().getAttribute("userDao");
+        userDao = (UserDao) getServletContext().getAttribute("storage");
         userService = (UserService) getServletContext().getAttribute("userService");
         jsonUtil = (JsonUtil) getServletContext().getAttribute("jsonUtil");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        log.info("UserServlet class - doGet() called");
         String usernameFromParameters = req.getParameter("username");
         User user = userService.getUserByUsername(usernameFromParameters);
         if (user != null) {
@@ -40,11 +43,13 @@ public class UserServlet extends HttpServlet {
             out.println(jsonUtil.jsonFromUser(user));
         } else {
             resp.sendError(404, "User not found");
+            log.error("Error in UserServlet class (doGet) - 404 NOT FOUND : unregistered user");
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        log.info("UserServlet class - doPost() called");
         BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
         User user = jsonUtil.jsonToUser(br.readLine());
 
@@ -56,30 +61,37 @@ public class UserServlet extends HttpServlet {
             if (userDao.createUser(user)) {
                 req.setAttribute("user", user);
                 resp.sendRedirect(req.getContextPath());
+                log.info("User has been created");
             }
         } else {
             resp.sendError(400, "Bad request");
+            log.error("Error in UserServlet class (doPost) - 400 BAD REQUEST : user has not been created");
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        log.info("UserServlet class - doPut() called");
         BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
         User modifiedUser = jsonUtil.jsonToUser(br.readLine());
 
         if (modifiedUser != null) {
             if (userDao.updateUser(req.getParameter("username"), modifiedUser)) {
+                log.info("User has been updated");
             }
         } else {
             resp.sendError(404, "Unregistered user");
+            log.error("Error in UserServlet class (doPut) - 404 NOT FOUND : unregistered user");
         }
     }
 
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
+        log.info("UserServlet class - doDelete() called");
         if (userDao.deleteUser(req.getParameter("username"))) {
+            log.info("User had been deleted");
         } else {
             resp.sendError(404, "Unregistered user");
+            log.error("Error in UserServlet class (doDelete) - 404 NOT FOUND : unregistered user");
         }
     }
 }
--- servlets/src/main/java/com/hillel/servlets/UsersServlet.java
+++ servlets/src/main/java/com/hillel/servlets/UsersServlet.java
@@ -3,6 +3,7 @@ package com.hillel.servlets;
 import com.hillel.model.User;
 import com.hillel.service.UserService;
 import com.hillel.util.JsonUtil;
+import lombok.extern.log4j.Log4j;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/users")
+@Log4j
 public class UsersServlet extends HttpServlet {
     private UserService userService;
 
@@ -23,8 +25,14 @@ public class UsersServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        log.info("UsersServlet class - doGet() called");
         List<User> users = userService.getListOfUsers();
-        JsonUtil toJson = new JsonUtil();
-        resp.getWriter().println(toJson.jsonFromUsersList(users));
+        if (users != null) {
+            JsonUtil toJson = new JsonUtil();
+            resp.getWriter().println(toJson.jsonFromUsersList(users));
+        } else {
+            resp.sendError(404, "User not found");
+            log.error("Error in UsersServlet class (doGet) - 404 NOT FOUND");
+        }
     }
 }
